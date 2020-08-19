7f9d74291735f909aeb3b518e094a53b8345507c
skudum <sushma.kudum@sap.com>
2020-08-19 19:11:21 +0530

    avatar examples added
    no serpator added
    
    morre button added
    
    signature updated
    
    test scripts added
    
    test specs added for datasource
    
    rebase with master

--- libs/platform/src/lib/domain/data-source.ts
+++ libs/platform/src/lib/domain/data-source.ts
@@ -163,6 +163,7 @@ export class ListDataSource<T> extends ComboBoxDataSource<T> {
         super(dataProvider);
     }
     // sort
+
     sort(listItems: [] | Map<string, string>): void {
         const sortedItems = new Map();
 
